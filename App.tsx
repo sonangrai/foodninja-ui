f7dcd4234078c34edd0edc004ca44efb0940e2a0
sonangrai <shonahangrae@gmail.com>
2024-02-27 09:14:57 +0545

    feat: added splash screen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,28 @@
 import Splash from "./components/Splash";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect, useState } from "react";
+import Home from "./components/screens/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Splash />;
+  const [splash, setSplash] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSplash(false);
+    }, 4000);
+  }, []);
+
+  // Splash screen for 3 sec
+  if (splash) return <Splash />;
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{ title: "Home" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }

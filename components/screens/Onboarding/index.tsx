61f7c9bab677bae66ecf8e52e8fb5471a3ee1a2f
sonangrai <shonahangrae@gmail.com>
2024-02-27 11:52:50 +0545

    feat: onboarding first

--- components/screens/Onboarding/index.tsx
+++ components/screens/Onboarding/index.tsx
@@ -1,12 +1,14 @@
 import { Image } from "react-native";
 import styled from "styled-components/native";
+import FoodButton from "../../../common/UI/FoodButton";
 
 function Onboarding() {
   return (
     <OnboardingView>
-      <Image source={require("../../../assets/onboard/Illustartion.png")} />
+      <Image source={require("../../../assets/onboard/illustration.png")} />
       <Heading>Find your Comfort {"\n"} Food here</Heading>
       <Info>Here You Can find a chef or dish for every {"\n"} taste and color. Enjoy!</Info>
+      <FoodButton title="Next" onPress={() => {}} />
     </OnboardingView>
   );
 }
@@ -22,8 +24,8 @@ const OnboardingView = styled.View`
 `;
 
 const Heading = styled.Text`
-  font-size: 22;
-  line-height: 40px;
+  font-size: 22px;
+  line-height: 36px;
   color: #09051c;
   text-align: center;
   font-weight: bold;
@@ -34,4 +36,5 @@ const Info = styled.Text`
   text-align: center;
   line-height: 24px;
   margin-top: 20px;
+  margin-bottom: 40px;
 `;
